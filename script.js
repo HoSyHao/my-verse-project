9dc3561148095be26804537831def12e4faf5ad7
HoSyHao <bloodarmour1@gmail.com>
2025-07-20 13:46:39 +0700

    Update undo-redo feature with new log

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Drag and drop feature in progress");
+console.log("Drag and drop feature updated by undo-redo branch");
 function dragImage() { console.log("Dragging image"); }
 dragImage();
 function undoAction() { console.log("Undo action"); }
