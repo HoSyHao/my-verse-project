7831b5cdac325abee5c103a81c8a589289c7e053
HoSyHao <bloodarmour1@gmail.com>
2025-07-20 13:32:48 +0700

    Add undo function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 console.log("Drag and drop feature in progress");
 function dragImage() { console.log("Dragging image"); }
 dragImage();
+function undoAction() { console.log("Undo action"); }
+undoAction();
