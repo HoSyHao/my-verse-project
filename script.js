934bf384ca93349f18e0edb026fc042d906225e1
HoSyHao <bloodarmour1@gmail.com>
2025-07-20 13:36:24 +0700

    Update dragImage function with new logic

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 console.log("Drag and drop feature in progress");
-function dragImage() { console.log("Dragging image"); }
+function dragImage() { console.log("Updated drag function"); }
 dragImage();
